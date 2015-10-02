cb22fcc5b53db81a037536fcd939768930bca726
Buddy Magsipoc <keikun17@gmail.com>
2015-10-02 23:59:59 +0800

    +Fish: alias fix。

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -2,7 +2,8 @@
 
 alias be "bundle exec"
 
+alias gst "git status"
+alias gs "git status"
+alias gd "git diff"
+alias grd "git rm (git ls-files --deleted)"
 # Git
-alias gst 'git status'
-alias gd 'git diff'
-alias grd 'git rm $(git ls-files --deleted)'
