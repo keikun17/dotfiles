d1a360254554483bd7e3c3577a4703a347a58ce9
Buddy Magsipoc <keikun17@gmail.com>
2015-10-03 00:14:11 +0800

    Aliases。

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,9 +1,30 @@
-# Bundler
+# Vim is nvim
+alias vim "nvim"
 
+# Bundler
 alias be "bundle exec"
 
-alias gst "git status"
-alias gs "git status"
-alias gd "git diff"
-alias grd "git rm (git ls-files --deleted)"
 # Git
+alias ga "git add -A"
+alias gcm "git commit -m"
+alias gd "git diff"
+alias gl "git log"
+alias gs "git status"
+alias grd "git rm (git ls-files --deleted)"
+
+# Dir
+alias lsg "ll | grep"
+
+# Fish configs
+alias falias "nvim ~/.config/fish/aliases.fish"
+alias fconfig "nvim ~/.config/fish/config.fish"
+
+# PS
+alias psa "ps aux"
+alias psg "ps aux | grep "
+alias psr 'ps aux | grep ruby'
+
+# Kill
+alias ka9='killall -9'
+alias k9='kill -9'
+
